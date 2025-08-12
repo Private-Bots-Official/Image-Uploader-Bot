8e9cf7555a3db9b9aa43a6cf3586d6f512667a88
Pʀɪᴍᴇ Hʀɪᴛᴜ <144017164+Prime-Hritu@users.noreply.github.com>
2025-08-12 14:19:36 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,8 +2,6 @@ import { serve } from "https://deno.land/std@0.195.0/http/server.ts";
 import { BotController } from "./controllers/bot.controller.ts";
 
 serve(async (req) => {
-  if (req.method !== "POST") return new Response("Method Not Allowed", { status: 405 });
-
   try {
     const update = await req.json();
     return await BotController.handleUpdate(update);

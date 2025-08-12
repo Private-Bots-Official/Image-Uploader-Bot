9fe93c54062314c991dd21d869ef683943c59043
Pʀɪᴍᴇ Hʀɪᴛᴜ <144017164+Prime-Hritu@users.noreply.github.com>
2025-08-12 14:22:09 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ import { serve } from "https://deno.land/std@0.195.0/http/server.ts";
 import { BotController } from "./controllers/bot.controller.ts";
 
 serve(async (req) => {
+  if (req.method !== "POST") return new Response("Success", { status: 200 });
   try {
     const update = await req.json();
     return await BotController.handleUpdate(update);
